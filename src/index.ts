0c7353a27aa9a6cfc2b0c10e00b940e871fe4a78
C <gitlab_admin_23dc13@example.com>
2026-07-10 21:59:58 +0800

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import * as __shared from './__shared';
-import * as exams from './exams';
-import * as manifest from './manifest';
-import * as catalog from './catalog';
-
-export const protocol = {
-	...__shared,
-	...exams,
-	...manifest,
-	...catalog,
-} as typeof __shared & typeof exams & typeof manifest & typeof catalog;
+export * from './__shared';
+export {
+	examDefs,
+	ExamCodeSchema,
+	SectionCodeSchema,
+	TaskCodeSchema,
+	ItemCodeSchema,
+	TaskContentSchema,
+	ItemContentSchema,
+	ResponseContentSchema,
+} from './exams';
+export { ManifestPaperSchema, ManifestSchema } from './manifest';
+export { CatalogPaperSchema, CatalogSchema } from './catalog';
