3cdc64339c593b6df3aa8518b66f69b5a4e9b0ac
C <gitlab_admin_23dc13@example.com>
2026-07-20 17:27:25 +0800

    WIP

--- src/exam/index.ts
+++ src/exam/index.ts
@@ -5,37 +5,36 @@ import toefl_ibt_20260121 from './toefl-ibt-20260121';
 export const ExamCodeSchema = z.enum([
 	...toefl_ibt_20260121.ExamCodeSchema.options,
 ]);
+export type ExamCode = z.infer<typeof ExamCodeSchema>;
 
 export const SectionCodeSchema = z.enum([
 	...toefl_ibt_20260121.SectionCodeSchema.options,
 ]);
+export type SectionCode = z.infer<typeof SectionCodeSchema>;
 
 export const TaskCodeSchema = z.enum([
 	...toefl_ibt_20260121.TaskCodeSchema.options,
 ]);
+export type TaskCode = z.infer<typeof TaskCodeSchema>;
 
 export const ItemCodeSchema = z.enum([
 	...toefl_ibt_20260121.ItemCodeSchema.options,
 ]);
+export type ItemCode = z.infer<typeof ItemCodeSchema>;
 
 export const TaskContentSchema = z.discriminatedUnion('taskCode', [
 	...toefl_ibt_20260121.TaskContentSchema.options,
 ]);
+export type TaskContent = z.infer<typeof TaskContentSchema>;
 
 export const ItemContentSchema = z.discriminatedUnion('itemCode', [
 	...toefl_ibt_20260121.ItemContentSchema.options,
 ]);
+export type ItemContent = z.infer<typeof ItemContentSchema>;
 
 export const ResponseContentSchema = z.discriminatedUnion('itemCode', [
 	...toefl_ibt_20260121.ResponseContentSchema.options,
 ]);
-
-export type ExamCode = z.infer<typeof ExamCodeSchema>;
-export type SectionCode = z.infer<typeof SectionCodeSchema>;
-export type TaskCode = z.infer<typeof TaskCodeSchema>;
-export type ItemCode = z.infer<typeof ItemCodeSchema>;
-export type TaskContent = z.infer<typeof TaskContentSchema>;
-export type ItemContent = z.infer<typeof ItemContentSchema>;
 export type ResponseContent = z.infer<typeof ResponseContentSchema>;
 
 /**
